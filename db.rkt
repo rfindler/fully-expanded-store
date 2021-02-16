e80efd5ba69da9b695b431300dd43973528755bb
Robby Findler <robby@racket-lang.org>
2021-02-16 13:54:52 -0600

    just getting used to sql

---
+++ db.rkt
@@ -0,0 +1,83 @@
+#lang racket
+(require db racket/runtime-path)
+(provide
+ (contract-out
+  [set-mapping!   (-> path-string? bytes? void?)]
+  [lookup (-> path-string? (or/c bytes? #f))])
+ in-db)
+
+(define-runtime-path db.sqlite "db.sqlite")
+
+(define-syntax-rule
+  (in-db stuff ...)
+  (in-db/proc (λ () stuff ...)))
+
+(define current-conn (make-parameter #f))
+
+(define (in-db/proc thunk)
+  (define conn #f)
+  (dynamic-wind
+   (λ () (set! conn (sqlite3-connect #:database db.sqlite)))
+   (λ () (parameterize ([current-conn conn]) (thunk)))
+   (λ () (disconnect conn) (set! conn #f))))
+
+(define (initialize-db)
+  (unless (file-exists? db.sqlite)
+    (define conn
+      (sqlite3-connect #:database db.sqlite #:mode 'create))
+    (query-exec
+     conn
+     "create table T (file string primary key, bytes string);")
+    (disconnect conn)))
+
+(define (set-mapping! filename bytes)
+  ;; start transaction
+  (define binding
+    (query
+     (get-conn)
+     "select bytes from T where file = $1;"
+     (path->bytes filename)))
+  (cond
+    [(null? (rows-result-rows binding))
+     (query-exec
+      (get-conn)
+      "insert into T(file, bytes) values ($1,$2);"
+      (path->bytes filename)
+      bytes)]
+    [else
+     (query-exec
+      (get-conn)
+      "update T set file = $1, bytes = $2;"
+      (path->bytes filename)
+      bytes)])
+  ;; end transaction
+  )
+
+(define(lookup filename)
+  (define the-rows
+    (rows-result-rows
+     (query
+      (get-conn)
+      "select bytes from T where file = $1;"
+      (path->bytes filename))))
+  (cond
+    [(null? the-rows) #f]
+    [else
+     (define the-row (list-ref the-rows 0))
+     (vector-ref the-row 0)]))
+
+(define (get-conn)
+  (define conn (current-conn))
+  (unless conn (error 'db.rkt "not in the dynamic-extent of in-db"))
+  conn)
+
+(module+ main
+  (initialize-db)  
+  (define tmp1.rkt (build-path "/Users/robby/tmp1.rkt"))
+  (define tmp2.rkt (build-path "/Users/robby/tmp2.rkt"))
+  (in-db
+   (set-mapping! tmp1.rkt #"abcdef")
+   (printf "~s\n" (lookup tmp2.rkt))
+   (printf "~s\n" (lookup tmp1.rkt))
+   (set-mapping! tmp1.rkt #"abcdefghi")
+   (printf "~s\n" (lookup tmp1.rkt))))
